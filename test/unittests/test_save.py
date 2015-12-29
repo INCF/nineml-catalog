4926cc73f057ad27d945244c96a41398a26db1d7
Tom Close <tclose@oist.jp>
2015-12-29 12:57:39 +1100

    updated property format to use Quantity elements

--- test/unittests/test_save.py
+++ test/unittests/test_save.py
@@ -1,7 +1,7 @@
 import os.path
 from unittest import TestCase
 import nineml
-from ninemlcatalog import lookup, save
+from ninemlcatalog import load, save
 
 
 class TestSave(TestCase):
@@ -9,19 +9,19 @@ class TestSave(TestCase):
     def test_liaf_round_trip(self):
         path = 'neuron/LeakyIntegrateAndFire'
         name =  'LeakyIntegrateAndFire'
-        liaf_doc = lookup(path)
-        liaf = lookup(path, name)
+        liaf_doc = load(path)
+        liaf = load(path, name)
         save(liaf, path, name)
-        reloaded_doc = lookup(path)
+        reloaded_doc = load(path)
         # FIXME: Should check a modified version of the model and then resave
         #        the original
         self.assertEqual(liaf_doc, reloaded_doc)
 
     def test_brunel_round_trip(self):
         path = 'network/Brunel2000/SR'
-        brunel = lookup(path)
+        brunel = load(path)
         save(brunel, path)
-        reloaded_brunel = lookup(path)
+        reloaded_brunel = load(path)
         # FIXME: Should check a modified version of the model and then resave
         #        the original        
         self.assertEqual(brunel, reloaded_brunel)
